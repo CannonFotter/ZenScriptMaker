5588a290cd26985ed54e8ee2ec208a52f7a881c5
CannonFotter <guofanyi49@126.com>
2016-03-06 13:13:51 +0800

    First release

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ logger.setLevel('DEBUG');
 var dir = process.cwd(),
   pluginDir = dir + '\\data\\',
   pluginList = [],
-  pluginData = [];
+  pluginPath = [];
 //插件化不可避
 //首先读取data下所有文件夹中的package.json
 logger.info("Starting ZenScript Maker...");
@@ -44,48 +44,99 @@ var settings = JSON.parse(fs.readFileSync(dir + "\\settings.json"));
 function getConfig(attr) {
   return settings[attr]
 }
-if (getConfig('address') == '127.0.0.1' || getConfig('address') == 'localhost') {
-  logger.info("Starting server... at port:" + getConfig('port'));
-  var zms = require('./server');
-  zms.startServer(getConfig('port'), logger);
-
-  logger.info("Loading all plugins...");
-  fs.readdir(pluginDir, function (err, files) {
-    if (err) {
-      logger.error(err);
-      throw err;
-    }
-    files.forEach(function (file) {
-      if (fs.statSync(pluginDir + file).isDirectory()) {
-        try {
-          fs.accessSync(pluginDir + file + '\\package.json');
-        } catch (e) {
-          logger.warn("\"" + pluginDir + file + '\\package.json' + "\" doesn't exist!");
-          return;
-        }
-        try {
-          var data = fs.readFileSync(pluginDir + file + '\\package.json');
-        } catch (e) {
-          logger.error(e);
-        }
-        data = JSON.parse(data);
-        pluginList.push(data);
-        //TODO
+logger.info("Loading all plugins...");
+fs.readdir(pluginDir, function (err, files) {
+  if (err) {
+    logger.error(err);
+    throw err;
+  }
+  files.forEach(function (file) {
+    if (fs.statSync(pluginDir + file).isDirectory()) {
+      try {
+        fs.accessSync(pluginDir + file + '\\package.json');
+      } catch (e) {
+        logger.warn("\"" + pluginDir + file + '\\package.json' + "\" doesn't exist!");
+        return;
       }
-    });
-    logger.info("Plugins loading completed");
-    //logger.debug(pluginList);
-    //logger.debug(JSON.stringify(pluginList));
-    ipcMain.on('request-mod-list', function (event, args){
-      logger.info(JSON.stringify(pluginList));
-      //返回json啊，雪藏
-      event.returnValue(1);
-    });
-    //
+      try {
+        var data = fs.readFileSync(pluginDir + file + '\\package.json');
+      } catch (e) {
+        logger.error(e);
+      }
+      data = JSON.parse(data);
+      pluginList[data.modid] = data;
+      pluginPath[data.modid] = pluginDir + file + '\\';
+    }
   });
-} else {
-  logger.info("Connecting to server... At " + getConfig('address') + ':' + getConfig('port'));
-}
+  logger.info("Plugins loading completed");
+  var modules = [];
+  //加载所有的module
+  var modList = [],
+    containerList = [],
+    pathList = [];
+  for (var key in pluginList) {
+    modList.push({
+      "modid": pluginList[key]['modid'],
+      "name": pluginList[key]['name']
+    });
+    containerList[pluginList[key]['modid']] = pluginList[key]['container'];
+    pathList[pluginList[key]['modid']] = pluginList[key]['pathname'] ? pluginList[key]['pathname'] : pluginList[key]['modid'];
+    //logger.info(pluginDir + pathList[key]+"\\"+pluginList[key]['script']);
+    var temp = pluginList[key]['container'];
+    for (var key1 in temp){
+      logger.info(pluginDir + pathList[key]+"\\"+temp[key1]['script']);
+      modules[key] = [];
+      //modules[key][temp[key1]['id']] = pluginDir + pathList[key]+"\\"+temp[key1]['script'];
+      modules[key][temp[key1]['id']] = pluginDir + pathList[key]+"\\"+temp[key1]['script'];
+    }
+
+  }
+  logger.info(pathList);
+  //events
+  ipcMain.on('get-mod-list', function (event) {
+    event.returnValue = modList;
+  });
+  ipcMain.on('get-container-list', function (event, arg) {
+    event.returnValue = containerList[arg];
+  });
+  ipcMain.on('get-item-list', function (event, arg) {
+    try {
+      var csv = fs.readFileSync(pluginPath[arg] + pluginList[arg]['item'], 'utf-8');
+    } catch (e) {
+      logger.error(e);
+    }
+    event.returnValue = csv;
+  });
+  //displayName注意！！！
+  //arg是数组["modid"=>"minecraft","name"=>"stone"]
+  //返回路径，生无可恋了
+  ipcMain.on('get-item-icon', function (event, arg) {
+    event.returnValue = pluginPath[arg['modid']] + pluginList[arg['modid']]['icon'] + '\\' + arg['name'];
+  });
+  ipcMain.on('get-path', function (event, arg) {
+    event.returnValue = pluginDir + pathList[arg]+"\\"
+  });
+  ipcMain.on('copy-to-clipboard', function (event, arg) {
+    const clipboard = require('electron').clipboard;
+    clipboard.writeText(arg, 'zenscriptmaker');
+    event.returnValue = true;
+  });
+  ipcMain.on('search', function (event, arg) {
+    event.returnValue = 1;
+  });
+  ipcMain.on('get-module-path', function (event, args) {
+    //0:modid
+    //1:container-id
+    //2:jquery
+    //3:opt
+    var zm = modules[args[0]][args[1]];
+    event.returnValue = zm;
+    //var zm = require(modules[args[0]][args[1]]);
+    //zm.init(args[2]);
+    //event.returnValue = zm.generate(args[3]);
+  });
+  //
+});
 
 createWindow();
 
@@ -99,7 +150,7 @@ function createWindow() {
   app.on('ready', function () {
     window = new BrowserWindow({width: 800, height: 600});
     //window.loadURL(getConfig('address') + ':' + getConfig('port'));
-    window.loadURL('http://127.0.0.1:12450/index.html');
+    window.loadURL('file:///' + dir + '\\old.html');
     //window.webContents.openDevTools();
   });
 }
--- js/craftingTable.js
+++ js/craftingTable.js
@@ -1,253 +1,252 @@
-$(document).ready(function () {
-  //初始化
-  $.event.special.copy.options.trustedDomains = ["*"];
-  $('#nav-box').load('./app/nav-box.html', function () {
-    bind();
-  });
-
-  //改变选中的槽
-  $('.btn-craft-group').change(function () {
-    //改选之后，要先恢复navbox默认的值或读取已有的值
-    //恢复navbox的状态
-    //局部刷新好了
-    $('#nav-box').load('./app/nav-box.html',
-      function () {
-        //重新绑定事件
-        bind();
-        //先看看选了什么
-        var slot = $('input[name=i]:checked');
-        if (slot.data('val')) {
-          changeicon();
-        }
-        if (slot.data('adv')) {
-          //如果之前有，先把数据转换成json对象
-          var json = $.parseJSON(slot.data('adv')),
-          //reuseDOM = $('#reuse'),
-            dN1DOM = $('#dFirst'),
-            dN2DOM = $('#dSecond'),
-            damageDOM = $('#advanceD'),
-            nbtDOM = $('#nbt-s'),
-            nbt_iDOM = $('#nbt-input'),
-          //trDOM = $('#transReplace'),
-            tr_iDOM = $('#transR-input');
-          console.log(json);
-          //暴力对照
-          $.each(json, function (k, v) {
-            //给下属选框选中
-            $("input[data-purpose='" + k + "']").trigger('click');
-            switch (k) {
-              //reuse没有下属选项，忽略
-              //-----------
-              //耐久相关
-              case 'anyDamage':
-              case 'onlyDamaged':
-              case 'transformDamage':
-              case 'withDamage':
-              case 'onlyDamageAtLeast':
-              case 'onlyDamageAtMost':
-                //
-                damageDOM.trigger('click');
-                if (v) dN1DOM.attr('value', v);
-                break;
-              case 'onlyDamageBetween':
-                //
-                damageDOM.trigger('click');
-                dN1DOM.attr('value', v[0]);
-                dN2DOM.attr('value', v[1]);
-                break;
-              //nbt
-              case 'withTag':
-              case 'onlyWithTag':
-                //
-                nbtDOM.trigger('click');
-                nbt_iDOM.html(v);
-                break;
-              case 'transformReplace':
-                //
-                //trDOM.trigger('click');
-                tr_iDOM.html(v);
-                break;
-            }
-          });
-        }
-      });
-  });
-});
-function readCSV(name) {
-  var path = './data/' + name + '/itempanel.csv';
-  $.get(path,
-    function (result) {
-      var r = $.csv.toArrays(result);
-      $('#item-list table').append('<tr><td>0</td> <td>0</td> <td>清空</td><td><a href="#" data-val="null">追加</a></td></tr>');
-      setTimeout(function () {
-        for (var i = 1; i < r.length; i++) {
-          $('#item-list table').append("<tr><td>" + r[i][1] + "</td><td>" + r[i][2] + "</td><td>" + r[i][4] + "</td><td><a href=\"#\" data-val=\"<" + r[i][0] + ":" + r[i][2] + ">\">追加</a></td></tr>");
-        }
-        if (i = r.length - 1) {
-          $('#item-list a').click(function () {
-            if ($('.btn-craft-group.active').length > 0) {
-              $('.btn-craft-group.active input').data('val', $(this).data('val'));
-            }
-          });
-        }
-      }, 0.2);
-    });
-}
-
-//绑定事件
-function bind() {
-  //自动显示/隐藏
-  $('input[name="a"]').click(function (e) {
-    $($(e.target).data('target')).toggle();
-  });
-
-  //修改耐久的数字选框是否可修改
-  $('input[name="damage"]').click(function (e) {
-    if ($(e.target).attr('type') == 'radio') {
-      if ($(e.target).attr('class') == 'oNumber') {
-        $('#dFirst').removeAttr('readonly');
-        $('#dSecond').attr('readonly', '');
-      } else if ($(e.target).attr('class') == 'tNumber') {
-        $('#dFirst, #dSecond').removeAttr('readonly');
-      } else {
-        $('#dFirst, #dSecond').attr('readonly', '');
-      }
-    }
-  });
-
-  //nav-box点击应用的事件
-  $('#submit').click(function () {
-    //先判断下改的是哪个槽
-    var slot = $('input[name=i]:checked');
-    if (slot.data('val') != 'null') {
-      //console.log(new Date().getTime());
-      var json = {};
-      //再看看都选择改什么了
-      var a = $('input[name="a"]:checked');
-      var arr = [];
-      //先循环获取所有purpose到一个数组，然后indexOf(...)
-      for (var i = 0; i < a.length; i++) {
-        arr[i] = $(a[i]).data('purpose');
-      }
-      //console.log(arr);
-      if (arr.indexOf('damage') != -1) {
-        //
-        //先获得选中的
-        var dchecked = $('input[name="damage"]:checked').data('purpose');
-        //判断数值是否合法
-        var s1 = $('#dFirst').val();
-        var s2 = $('#dSecond').val();
-        if (!/^\d+$/.test(s1) || !/^\d+$/.test(s2) || s1 < 0 || s2 < 0) {
-          alert('参数必须是非负整数');
-          return false;
-        }
-        switch (dchecked) {
-          case 'transformDamage':
-          case 'withDamage':
-          case 'onlyDamageAtLeast':
-          case 'onlyDamageAtMost':
-            //此处需要参数1的值来做参数
-            //给选中的槽的data-adv赋值(先存到缓存里)
-            json[dchecked] = s1;
-            break;
-          case 'anyDamage':
-          case 'onlyDamaged':
-            //此处不需要参数
-            json[dchecked] = '';
-            break;
-          case 'onlyDamageBetween':
-            //此处需要参数1和参数2
-            if (s1 > s2) {
-              alert('参数1必须小于参数2');
-              return false;
-            }
-            json[dchecked] = {0: s1, 1: s2};
-            break;
-        }
-        //再看看参数
-      }
-      if (arr.indexOf('nbt') != -1) {
-        var nbt = $('#nbt-input').val();
-        if (nbt) {
-          json[$('input[name="nbt"]:checked').data('purpose')] = nbt;
-        }
-      }
-      if (arr.indexOf('transformReplace') != -1) {
-        var transReplace = $('#transR-input').val();
-        if (transReplace) {
-          json['transformReplace'] = transReplace;
-        }
-      }
-      if (arr.indexOf('reuse') != -1) {
-        json['reuse'] = '';
-      }
-      slot.data('adv', JSON.stringify(json));
-    }
-  });
-}
-
-$.getJSON('./data/mods.json',
-  function (data) {
-    $.each(data,
-      function (k, v) {
-        $('#mod-select').append('<option value="' + k + '">' + v + '</option>');
-      });
-  });
-readCSV('0-mc');
-
-//修改item列表
-$('#mod-select').change(function () {
-  //console.log($(this).val());
-  $('#item-list tbody').html('');
-  readCSV($(this).val());
-});
-
-//zeroclipboard复制数据
-$('#copy').on("copy",
-  function (e) {
-    e.clipboardData.clearData();
-    e.clipboardData.setData("text/plain", $('#output').val());
-    e.preventDefault();
-  });
-
-//主要按钮点击事件
-$('#toolbar2 button').click(function () {
-  if ($(this).data('purpose') != '') {
-    switch ($(this).data('purpose')) {
-      case 'copy':
-        //$('#copy').trigger();
-        break;
-      case 'save':
-        var fs = require('fs');
-        var filename = prompt("请选择保存的文件名","请输入文件名(不含扩展名)");
-        fs.writeFile('./output/' + filename + '.zs', $('#copyright').val() + $('#output').val(),
-          function (err) {
-            if (err) {
-              alert('写入出错，' + err);
-            } else {
-              alert('写入成功');
-            }
-          });
-        break;
-      case 'append':
-        //console.log($('#output'));
-        var craftTable = require('./js/zm.craftingtable');
-        craftTable.init(jQuery);
-        var str = $('#output').val();
-        if (str) {
-          $('#output').val(str+"\r\n"+craftTable.generate($('input[name=r2]:checked').val()));
-        }else{
-          $('#output').val(craftTable.generate($('input[name=r2]:checked').val()));
-        }
-            break;
-    }
-  }
-
-});
-
-function changeicon() {
-
-}
-/*
- Created by ZenScriptMaker<https://www.github.com/CannonFotter/ZenScriptMaker>
- */
+//$(document).ready(function () {
+//  //初始化
+//  $.event.special.copy.options.trustedDomains = ["*"];
+//  $('#nav-box').load('./app/nav-box.html', function () {
+//    bind();
+//  });
+//
+//  //改变选中的槽
+//  $('.btn-craft-group').change(function () {
+//    //改选之后，要先恢复navbox默认的值或读取已有的值
+//    //恢复navbox的状态
+//    //局部刷新好了
+//    $('#nav-box').load('./app/nav-box.html',
+//      function () {
+//        //重新绑定事件
+//        bind();
+//        //先看看选了什么
+//        var slot = $('input[name=i]:checked');
+//        if (slot.data('val')) {
+//          changeicon();
+//        }
+//        if (slot.data('adv')) {
+//          //如果之前有，先把数据转换成json对象
+//          var json = $.parseJSON(slot.data('adv')),
+//          //reuseDOM = $('#reuse'),
+//            dN1DOM = $('#dFirst'),
+//            dN2DOM = $('#dSecond'),
+//            damageDOM = $('#advanceD'),
+//            nbtDOM = $('#nbt-s'),
+//            nbt_iDOM = $('#nbt-input'),
+//          //trDOM = $('#transReplace'),
+//            tr_iDOM = $('#transR-input');
+//          console.log(json);
+//          //暴力对照
+//          $.each(json, function (k, v) {
+//            //给下属选框选中
+//            $("input[data-purpose='" + k + "']").trigger('click');
+//            switch (k) {
+//              //reuse没有下属选项，忽略
+//              //-----------
+//              //耐久相关
+//              case 'anyDamage':
+//              case 'onlyDamaged':
+//              case 'transformDamage':
+//              case 'withDamage':
+//              case 'onlyDamageAtLeast':
+//              case 'onlyDamageAtMost':
+//                //
+//                damageDOM.trigger('click');
+//                if (v) dN1DOM.attr('value', v);
+//                break;
+//              case 'onlyDamageBetween':
+//                //
+//                damageDOM.trigger('click');
+//                dN1DOM.attr('value', v[0]);
+//                dN2DOM.attr('value', v[1]);
+//                break;
+//              //nbt
+//              case 'withTag':
+//              case 'onlyWithTag':
+//                //
+//                nbtDOM.trigger('click');
+//                nbt_iDOM.html(v);
+//                break;
+//              case 'transformReplace':
+//                //
+//                //trDOM.trigger('click');
+//                tr_iDOM.html(v);
+//                break;
+//            }
+//          });
+//        }
+//      });
+//  });
+//});
+//function readCSV(name) {
+//  var path = './data/' + name + '/itempanel.csv';
+//  $.get(path,
+//    function (result) {
+//      var r = $.csv.toArrays(result);
+//      $('#item-list table').append('<tr><td>0</td> <td>0</td> <td>清空</td><td><a href="#" data-val="null">追加</a></td></tr>');
+//      setTimeout(function () {
+//        for (var i = 1; i < r.length; i++) {
+//          $('#item-list table').append("<tr><td>" + r[i][1] + "</td><td>" + r[i][2] + "</td><td>" + r[i][4] + "</td><td><a href=\"#\" data-val=\"<" + r[i][0] + ":" + r[i][2] + ">\">追加</a></td></tr>");
+//        }
+//        if (i = r.length - 1) {
+//          $('#item-list a').click(function () {
+//            if ($('.btn-craft-group.active').length > 0) {
+//              $('.btn-craft-group.active input').data('val', $(this).data('val'));
+//            }
+//          });
+//        }
+//      }, 0.2);
+//    });
+//}
+//
+////绑定事件
+//function bind() {
+//  //自动显示/隐藏
+//  $('input[name="a"]').click(function (e) {
+//    $($(e.target).data('target')).toggle();
+//  });
+//
+//  //修改耐久的数字选框是否可修改
+//  $('input[name="damage"]').click(function (e) {
+//    if ($(e.target).attr('type') == 'radio') {
+//      if ($(e.target).attr('class') == 'oNumber') {
+//        $('#dFirst').removeAttr('readonly');
+//        $('#dSecond').attr('readonly', '');
+//      } else if ($(e.target).attr('class') == 'tNumber') {
+//        $('#dFirst, #dSecond').removeAttr('readonly');
+//      } else {
+//        $('#dFirst, #dSecond').attr('readonly', '');
+//      }
+//    }
+//  });
+//
+//  //nav-box点击应用的事件
+//  $('#submit').click(function () {
+//    //先判断下改的是哪个槽
+//    var slot = $('input[name=i]:checked');
+//    if (slot.data('val') != 'null') {
+//      //console.log(new Date().getTime());
+//      var json = {};
+//      //再看看都选择改什么了
+//      var a = $('input[name="a"]:checked');
+//      var arr = [];
+//      //先循环获取所有purpose到一个数组，然后indexOf(...)
+//      for (var i = 0; i < a.length; i++) {
+//        arr[i] = $(a[i]).data('purpose');
+//      }
+//      //console.log(arr);
+//      if (arr.indexOf('damage') != -1) {
+//        //
+//        //先获得选中的
+//        var dchecked = $('input[name="damage"]:checked').data('purpose');
+//        //判断数值是否合法
+//        var s1 = $('#dFirst').val();
+//        var s2 = $('#dSecond').val();
+//        if (!/^\d+$/.test(s1) || !/^\d+$/.test(s2) || s1 < 0 || s2 < 0) {
+//          alert('参数必须是非负整数');
+//          return false;
+//        }
+//        switch (dchecked) {
+//          case 'transformDamage':
+//          case 'withDamage':
+//          case 'onlyDamageAtLeast':
+//          case 'onlyDamageAtMost':
+//            //此处需要参数1的值来做参数
+//            //给选中的槽的data-adv赋值(先存到缓存里)
+//            json[dchecked] = s1;
+//            break;
+//          case 'anyDamage':
+//          case 'onlyDamaged':
+//            //此处不需要参数
+//            json[dchecked] = '';
+//            break;
+//          case 'onlyDamageBetween':
+//            //此处需要参数1和参数2
+//            if (s1 > s2) {
+//              alert('参数1必须小于参数2');
+//              return false;
+//            }
+//            json[dchecked] = {0: s1, 1: s2};
+//            break;
+//        }
+//        //再看看参数
+//      }
+//      if (arr.indexOf('nbt') != -1) {
+//        var nbt = $('#nbt-input').val();
+//        if (nbt) {
+//          json[$('input[name="nbt"]:checked').data('purpose')] = nbt;
+//        }
+//      }
+//      if (arr.indexOf('transformReplace') != -1) {
+//        var transReplace = $('#transR-input').val();
+//        if (transReplace) {
+//          json['transformReplace'] = transReplace;
+//        }
+//      }
+//      if (arr.indexOf('reuse') != -1) {
+//        json['reuse'] = '';
+//      }
+//      slot.data('adv', JSON.stringify(json));
+//    }
+//  });
+//}
+//
+//$.getJSON('./data/mods.json',
+//  function (data) {
+//    $.each(data,
+//      function (k, v) {
+//        $('#mod-select').append('<option value="' + k + '">' + v + '</option>');
+//      });
+//  });
+//readCSV('0-mc');
+//
+////修改item列表
+//$('#mod-select').change(function () {
+//  //console.log($(this).val());
+//  $('#item-list tbody').html('');
+//  readCSV($(this).val());
+//});
+//
+////zeroclipboard复制数据
+//$('#copy').on("copy", function (e) {
+//    e.clipboardData.clearData();
+//    e.clipboardData.setData("text/plain", $('#output').val());
+//    e.preventDefault();
+//});
+//
+////主要按钮点击事件
+//$('#toolbar2 button').click(function () {
+//  if ($(this).data('purpose') != '') {
+//    switch ($(this).data('purpose')) {
+//      case 'copy':
+//        //$('#copy').trigger();
+//        break;
+//      case 'save':
+//        var fs = require('fs');
+//        var filename = prompt("请选择保存的文件名","请输入文件名(不含扩展名)");
+//        fs.writeFile('./output/' + filename + '.zs', $('#copyright').val() + $('#output').val(),
+//          function (err) {
+//            if (err) {
+//              alert('写入出错，' + err);
+//            } else {
+//              alert('写入成功');
+//            }
+//          });
+//        break;
+//      case 'append':
+//        //console.log($('#output'));
+//        var craftTable = require('./js/zm.craftingtable');
+//        craftTable.init(jQuery);
+//        var str = $('#output').val();
+//        if (str) {
+//          $('#output').val(str+"\r\n"+craftTable.generate($('input[name=r2]:checked').val()));
+//        }else{
+//          $('#output').val(craftTable.generate($('input[name=r2]:checked').val()));
+//        }
+//            break;
+//    }
+//  }
+//
+//});
+//
+//function changeicon() {
+//
+//}
+///*
+// Created by ZenScriptMaker<https://www.github.com/CannonFotter/ZenScriptMaker>
+// */
